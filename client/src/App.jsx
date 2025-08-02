8a09bbbed0205d9ec9f598ddbe9067ade370f56d
Pariharx7 <parihar.peace@gmail.com>
2025-08-02 15:30:23 +0530

    xd

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,8 @@ import RegistrationPage from './pages/RegistrationPage';
 import LoginPage from './pages/LoginPage';
 import IssueDetailPage from './pages/IssueDetailPage';
 import ReportIssuePage from './pages/ReportIssuePage';
-import SearchResultsPage from './pages/SearchResultsPage'; // If separate page
+import MyIssuesPage from './pages/MyIssuesPage';
+import IssuesListPage from './pages/IssuesListPage';
 
 function App() {
   return (
@@ -16,8 +17,9 @@ function App() {
       <Route path="/register" element={<RegistrationPage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/report-issue" element={<ReportIssuePage />} />
-      <Route path="/search" element={<SearchResultsPage />} />
+      <Route path="/search" element={<IssuesListPage />} />
       <Route path="/issue/:id" element={<IssueDetailPage />} /> {/* Example for dynamic ID */}
+      <Route path="/my-issues" element={<MyIssuesPage />} />
       {/* <Route path="*" element={<NotFoundPage />} /> */}
       {/* Add more routes as per your application's flow */}
     </Routes>
