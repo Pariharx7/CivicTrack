1cefc1fd3d8af918c80bf0257fe18e3ac5df74ff
codeagrim <agrim32sharma@gmail.com>
2025-08-02 13:49:55 +0530

    feat: Configure database connection

--- Server/src/app.js
+++ Server/src/app.js
@@ -1,16 +1,16 @@
-import express from "express";
-import dotenv from "dotenv";
+import express, { json } from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 
-dotenv.config();
 
-const app  = express();
-
+const app = express()
 
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
+
+
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
@@ -18,15 +18,16 @@ app.use(cors({
 }))
 
 app.use(express.json({limit:"16kb"}))
-
 app.use(express.static("public"))
-
 app.use(cookieParser())
 
 
-app.get('/', (req,res) =>{
-    res.send("We Are Live")
-})
+
+
+import router from "./routes/auth.routes.js";
+
+app.use('/api/auth', router)
+
 
 
 export default app;
--- Server/src/index.js
+++ Server/src/index.js
@@ -1,6 +1,19 @@
 import app from "./app.js";
+import dotenv from "dotenv";
+import dbconnect from "./config/db.js";
 
 
+
+dotenv.config();
+
+
+// Db Connection
+dbconnect().then(
 app.listen(process.env.PORT || 8000, () => {
     console.log(`Server Started on Port http://localhost:${process.env.PORT}`)
 })
+)
+.catch((err)=>{
+        console.log(`MongoDB ERROR ${err}`)
+    
+    })
